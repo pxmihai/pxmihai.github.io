dd839df0397846a7ad53696ec85a6be3ef06e8f7
pxmihai <pxmihai@yahoo.com>
2021-05-27 12:42:49 +0300

    small change

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            Changes showing <p>  => gh-pages </p>
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Changes showing <p> => gh-pages </p>
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
