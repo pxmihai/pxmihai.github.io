93345eee400e46b3823affc42cebea412b9cc4d9
pxmihai <pxmihai@yahoo.com>
2022-08-21 12:26:13 +0300

    - small change

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
                 <img src={logo} className="App-logo" alt="logo"/>
                 <p>
                     Changes showing <p> => gh-pages </p>
+                    <p>Working!</p>
                 </p>
             </header>
         </div>
