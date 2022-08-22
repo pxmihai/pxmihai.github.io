8cfeec9c88b8b3119b6aa72409454848629735be
pxmihai <pxmihai@yahoo.com>
2022-08-22 13:49:43 +0300

    - smol change

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 clear
+
+1. ## npm run deploy
+https://favicon.io/favicon-generator/
+https://dev.to/yuribenjamin/how-to-deploy-react-app-in-github-pages-2a1f
--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
                 <p>
-                    Changes showing <p> => gh-pages </p>
-                    <p>Working!</p>
+                    Hello world :)
                 </p>
+                {/*<img src={logo} className="App-logo" alt="logo"/>*/}
+
             </header>
         </div>
     );
